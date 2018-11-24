c1e837d2ead109dfdace6157cb7e920b22ea46e3
mikaelaatfrantic <mikaela.hallenberg@frantic.com>
2018-11-24 13:15:14 +0200

    Create vue front end

---
+++ app.vue
@@ -0,0 +1,34 @@
+
+<template>
+  <div id="app">
+  </div>
+</template>
+<script>
+// import GroupInfoContainer from './components/group-info/group-info-container';
+
+export default {
+  name: 'App',
+  store,
+  components: {
+    // 'group-info': GroupInfoContainer,
+
+  },
+  computed: {
+    groupExists() {
+      return this.groupMembers && this.groupMembers.length > 0;
+    },
+  },
+  methods: {
+    isOnlyAdult() {
+      const groupAdults = this.groupMembers.filter(user => user.juniorFlag === 'N');
+      return this.groupMembers.length === 1 || groupAdults.length === 1;
+    },
+  },
+  created() {
+    if (!store.state.groupInfo || !store.state.groupInfo.length) {
+      store.dispatch('START_LOADING_GROUP_DATA');
+    }
+  },
+};
+</script>
+
