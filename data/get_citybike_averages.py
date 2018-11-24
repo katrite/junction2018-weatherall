56789e032d5791934b31b399fc2dc03778282260
Aleksi Pekkala <aleksi.v.a.pekkala@student.jyu.fi>
2018-11-24 18:02:56 +0200

    Handle malformed JSON in citybike script

--- data/get_citybike_averages.py
+++ data/get_citybike_averages.py
@@ -21,21 +21,40 @@ def files_per_hour(month, day, hour):
 def hour_stats(month, day, hour):
     res = {}
     for f in files_per_hour(month, day, hour):
-        stations = json.load(open(f, "r"))["result"]
-        for s in stations:
-            key = s["name"]
-            if not key in res:
-                res[key] = {
-                    "total_slots": s["total_slots"],
-                    "free_slots": [],
-                    "avl_bikes": [],
-                }
-            res[key]["free_slots"].append(s["free_slots"])
-            res[key]["avl_bikes"].append(s["avl_bikes"])
+        try:
+            stations = json.load(open(f, "r"))["result"]
+            for s in stations:
+                key = s["name"]
+                if not key in res:
+                    res[key] = {
+                        "total_slots": s["total_slots"],
+                        "free_slots": [],
+                        "avl_bikes": [],
+                    }
+                res[key]["free_slots"].append(s["free_slots"])
+                res[key]["avl_bikes"].append(s["avl_bikes"])
+        except json.JSONDecodeError:
+            print("ERROR: invalid JSON at", f)
+            continue
+        except KeyError:
+            print("ERROR: invalid format at", f)
+            continue
 
     for k, v in res.items():
         res[k]["free_slots"] = sum(res[k]["free_slots"]) // len(res[k]["free_slots"])
         res[k]["avl_bikes"] = sum(res[k]["avl_bikes"]) // len(res[k]["avl_bikes"])
 
     return [{"name": k, **v} for k, v in res.items()]
+
+
+def main():
+    for month in [9, 10]:
+        days_n = 30 if month == 9 else 31
+        for day in range(1, days_n + 1):
+            for hour in range(0, 24):
+                h_stats = hour_stats(month, day, hour)
+                out_f_name = f"./citybike_averages/citybikes-2018-{str(month).zfill(2)}-{str(day).zfill(2)}:{str(hour).zfill(2)}.json"
+                print("writing", out_f_name, "stations n:", len(h_stats))
+                with open(out_f_name, "w") as out_f:
+                    json.dump(h_stats, out_f)
 
